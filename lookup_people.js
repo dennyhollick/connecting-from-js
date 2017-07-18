8812f7b8fb0b74d216df43dd7e5d3e03f71df371
Denny Hollick <denny@Dennys-MacBook-Pro.local>
2017-07-18 13:43:55 -0700

    Linted code :)

--- lookup_people.js
+++ lookup_people.js
@@ -1,47 +1,53 @@
-const pg = require("pg");
-const settings = require("./settings"); // settings.json
+const pg = require('pg');
+const settings = require('./settings');
+ // settings.json
 const lookup = process.argv[2].toUpperCase();
 console.log(lookup);
 
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl,
 });
+
+// HELPER FUNCTIONS
+
+
+function convertDateTimeToDate(dateTime) {
+  const day = (`0${dateTime.getDate()}`).slice(-2);
+  const month = (`0${dateTime.getMonth() + 1}`).slice(-2);
+  const year = dateTime.getFullYear();
+
+  return (`${year}-${month}-${day}`);
+}
+
+function logRows(tableRowsArray) {
+  let index = 1;
+  tableRowsArray.forEach((row) => {
+    console.log(`- ${index}: ${row.first_name} ${row.last_name}, born ${convertDateTimeToDate(row.birthdate)}`);
+    index += 1;
+  });
+}
+
+// CLIENT QUERY
 
 client.connect((err) => {
   if (err) {
-    return console.error("Connection Error", err);
+    return console.error('Connection Error', err);
   }
-  console.log('Searching...')
+  console.log('Searching...');
   client.query(`SELECT first_name, last_name, birthdate
                   FROM famous_people 
-                  WHERE (UPPER(first_name) LIKE '${lookup}' OR UPPER(last_name) LIKE '${lookup}');`, 
+                  WHERE (UPPER(first_name) LIKE '${lookup}' OR UPPER(last_name) LIKE '${lookup}');`,
     (err, result) => {
       if (err) {
-        return console.error("error running query", err);
+        return console.error('error running query', err);
       }
       console.log(`Found ${result.rows.length} person(s) by the name of ${lookup}`);
       logRows(result.rows);
       client.end();
     });
 });
-
-function logRows (tableRowsArray) {
-  index = 1;
-  tableRowsArray.forEach(function(row) {
-    console.log(`- ${index}: ${row.first_name} ${row.last_name}, born ${convertDateTimeToDate(row.birthdate)}`);
-    index++;
-  });
-}
-
-function convertDateTimeToDate (dateTime) {
-  const day = ("0" + dateTime.getDate()).slice(-2)
-  const month = ("0" + (dateTime.getMonth() + 1)).slice(-2)
-  const year = dateTime.getFullYear();
-
-  return (`${year}-${month}-${day}`)
-}
