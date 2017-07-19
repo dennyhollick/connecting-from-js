3b24b47075892eb5b8a6489e6c55ccc615db1f9e
Denny Hollick <denny@Dennys-MacBook-Pro.local>
2017-07-19 10:23:10 -0700

    changed pg to knex

--- lookup_people.js
+++ lookup_people.js
@@ -1,17 +1,19 @@
-const pg = require('pg');
 const settings = require('./settings');
- // settings.json
-const lookup = process.argv[2].toUpperCase();
-console.log(lookup);
+// settings.json
 
-const client = new pg.Client({
-  user: settings.user,
-  password: settings.password,
-  database: settings.database,
-  host: settings.hostname,
-  port: settings.port,
-  ssl: settings.ssl,
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    database: settings.database,
+    user: settings.user,
+    host: settings.hostname,
+    port: settings.port,
+    ssl: settings.ssl,
+    password: settings.password,
+  },
 });
+
+const lookup = process.argv[2];
 
 // HELPER FUNCTIONS
 
@@ -34,20 +36,16 @@ function logRows(tableRowsArray) {
 
 // CLIENT QUERY
 
-client.connect((err) => {
-  if (err) {
-    return console.error('Connection Error', err);
-  }
-  console.log('Searching...');
-  client.query(`SELECT first_name, last_name, birthdate
-                  FROM famous_people 
-                  WHERE (UPPER(first_name) LIKE '${lookup}' OR UPPER(last_name) LIKE '${lookup}');`,
-    (err, result) => {
-      if (err) {
-        return console.error('error running query', err);
-      }
-      console.log(`Found ${result.rows.length} person(s) by the name of ${lookup}`);
-      logRows(result.rows);
-      client.end();
-    });
-});
+
+knex.select('first_name', 'last_name', 'birthdate')
+  .from('famous_people')
+  .where('first_name', 'LIKE', lookup)
+  .orWhere('last_name', 'LIKE', lookup)
+  .asCallback((err, result) => {
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(`Found ${result.length} person(s) by the name of ${lookup}`);
+    logRows(result);
+    knex.destroy();
+  });
